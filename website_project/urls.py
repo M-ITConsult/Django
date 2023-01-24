51f64b415cab4e1def99e4ae1aea0358c382c2f1
m-itconsult <jchoro74@gmail.com>
2023-01-24 20:25:15 +0100

    update

--- website_project/urls.py
+++ website_project/urls.py
@@ -18,10 +18,13 @@ from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('learning_logs.urls')),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root':settings.STATIC_ROOT}),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
