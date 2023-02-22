ada85a56dfbd6fa46b1942a7a44f59fa6fd84e4c
m-itconsult <jchoro74@gmail.com>
2023-02-22 21:20:27 +0100

    update

--- website_project/settings.py
+++ website_project/settings.py
@@ -28,8 +28,8 @@ DEBUG = 'RENDER' not in os.environ
 
 # Email informations for form
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '$EMAIL'
-EMAIL_HOST_PASSWORD = '$GMAIL_PASSWORD'
+EMAIL_HOST_USER = 'EMAIL'
+EMAIL_HOST_PASSWORD = 'GMAIL_PASSWORD'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
