49386162287ba681d63af6db98b27b841a73bc47
m-itconsult <jchoro74@gmail.com>
2023-02-22 20:06:26 +0100

    test

--- website_project/settings.py
+++ website_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h6i=9$ms-mhy4y@=_%wa@r+845wa1jvvxokycz2wmm=n0ppcu1'
+SECRET_KEY = '$SECRET_KEY_DJANGO'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
@@ -29,7 +29,7 @@ DEBUG = 'RENDER' not in os.environ
 # Email informations for form
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'jchoro74@gmail.com'
-EMAIL_HOST_PASSWORD = 'fbmogdrrlmbckmen'
+EMAIL_HOST_PASSWORD = '$PASSWORD_GMAIL'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
