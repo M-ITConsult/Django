a5dab6c0fed513073a7618308614982e0de8a766
M-ITConsult <jchoro74@gmail.com>
2023-09-03 10:43:09 +0200

    update

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -7,6 +7,6 @@ from . import views
 app_name = 'learning_logs'
 urlpatterns = [
     # Home page
-    path('contact/', views.index, name='contact'),
+    path('', views.index, name='contact'),
     
 ]
